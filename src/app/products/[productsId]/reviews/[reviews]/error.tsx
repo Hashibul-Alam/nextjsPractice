217c7f3eab5b7c97adf2164b8306d6e7c215c0ab
sha2311 <sha31417@gmail.com>
2025-02-22 12:26:48 +0600

    nested

--- src/app/products/[productsId]/reviews/[reviews]/error.tsx
+++ src/app/products/[productsId]/reviews/[reviews]/error.tsx
@@ -1,11 +1,16 @@
 "use client"
 import React from 'react'
 
-const ErrorPage = ({error}:{
-  error: Error
+const ErrorPage = ({error, reset}:{
+  error: Error;
+  reset : ()=> void;
+
 }) => {
   return (
-    <div> {error.message}</div>
+    <div> 
+      <p>{error.message}</p>
+      <button className='bg-green-600 px-3 py-1 rounded text-white ms-2' onClick={reset} >Try Again</button>
+    </div>
   )
 }
 
